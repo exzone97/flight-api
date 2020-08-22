01a8fc39030d2a614b952f6c1a3db4893fe32894
Jonathan Alva <jonathan.alva97@yahoo.com>
2020-08-22 14:12:00 +0700

    Adding Route to get all flight by airlinecode.

--- routes/flights.js
+++ routes/flights.js
@@ -27,7 +27,24 @@ function getDateFromISOString(ISOString) {
     // Use split to get date from isostring date
     return ISOString.split('T')[0];
 }
-
+/**
+ * Function to get Subset from a Flight Number
+ * @param {*} flightNumber 
+ * return subset from a flight number
+ */
+function getSubsetFlightNumber(flightNumber) {
+    return flightNumber.substring(0, 2)
+}
+/**
+ * Function to get all flight with subset
+ * @param {*} subset 
+ */
+function getFlightWithSubset(subset) {
+    let flights = getFlightData().filter(function (flight) {
+        return getSubsetFlightNumber(flight.flightNumber).toLowerCase() == subset;
+    })
+    return flights
+}
 /**
  * Function to get all flight has arrival date = today.
  * Return: JSON contain all flight that has arrival date = today
@@ -42,6 +59,10 @@ function getFlightArrivalToday() {
     return flights
 }
 
+/**
+ * Router get / will return JSON for every flight that has arrivaltime today.
+ * If there any flights will return JSON but if there no flight will return status 404 - Not Found
+ */
 router.get('/', async function (req, res, next) {
     try {
         let flights = getFlightArrivalToday();
@@ -60,6 +81,36 @@ router.get('/', async function (req, res, next) {
         }
     } catch (err) {
         console.log(err)
+        res.status(400).json({
+            'status': 'ERROR',
+            'messages': err.messages,
+            'data': []
+        })
+    }
+});
+
+/**
+ * Route to get all flight that have airlinecode.
+ */
+router.get('/:airlineCode', async function (req, res, next) {
+    try {
+        // Get Parameter and set the parameter to lower case
+        let airlinecode = req.params.airlineCode.toLowerCase();
+        let flights = getFlightWithSubset(airlinecode)
+        if (flights.length > 0) {
+            res.status(200).json({
+                'status': 'OK',
+                'messages': "",
+                'data': flights
+            })
+        } else {
+            res.status(404).json({
+                'status': 'ERROR',
+                'messages': 'Flight Not Found',
+                'data': []
+            })
+        }
+    } catch (err) {
         res.status(400).json({
             'status': 'ERROR',
             'messages': err.messages,
